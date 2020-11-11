4e580b2b05739093ab80716ce33c026d8b137b97
Per Lindgren <per.lindgren@ltu.se>
2020-11-11 22:36:08 +0100

    production v1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 //! examples/init.rs
 
 #![deny(unsafe_code)]
-#![deny(warnings)]
+// #![deny(warnings)]
 #![no_main]
 #![no_std]
 
-use cortex_m_semihosting::hprintln;
-use panic_semihosting as _;
+use cortex_m;
+// use panic_halt as _;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
 use stm32f4;
 
 // #[rtic::app(device = lm3s6965, peripherals = true)]
@@ -14,17 +16,36 @@ use stm32f4;
 const APP: () = {
     #[init]
     fn init(cx: init::Context) {
-        static mut X: u32 = 0;
-
         // Cortex-M peripherals
         let _core: cortex_m::Peripherals = cx.core;
 
         // Device specific peripherals
         // let _device: lm3s6965::Peripherals = cx.device;
 
-        // Safe access to local `static mut` variable
-        let _x: &'static mut u32 = X;
+        // // Safe access to local `static mut` variable
+        // let _x: &'static mut u32 = X;
+        rtt_init_print!();
+        rprintln!("init");
+    }
 
-        hprintln!("init").unwrap();
+    #[idle]
+    fn idle(_cx: idle::Context) -> ! {
+        //     // static mut X: u32 = 0;
+
+        //     // // Cortex-M peripherals
+        //     // let _core: cortex_m::Peripherals = cx.core;
+
+        //     // // Device specific peripherals
+        //     // // let _device: lm3s6965::Peripherals = cx.device;
+
+        //     // // Safe access to local `static mut` variable
+        //     // let _x: &'static mut u32 = X;
+
+        rprintln!("idle");
+        panic!("panic");
+        loop {
+            continue;
+            // cortex_m::asm::wfi(); // does not work for some reason
+        }
     }
 };
